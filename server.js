0fe46a777e98c79994af021270199be691d95c17
skkad <skkad@gmail.com>
2023-11-29 23:47:39 +0530

    new details api and cors

--- server.js
+++ server.js
@@ -11,16 +11,23 @@ const validateLogin = require("./middleware/auth");
 const app = express();
 app.use(express.json());
 
-app.use(function (req, res, next) {
-  let allowedOrigins = ["http://localhost:3000/", "*"];
-  let origin = req.headers.origin;
-  if (allowedOrigins.includes(origin)) {
-    res.header("Access-Control-Allow-Origin", origin);
-  }
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin,X-Requested-With,Content-Type,Accept"
-  );
+// app.use(function (req, res, next) {
+//   let allowedOrigins = ["http://localhost:3000/", "*"];
+//   let origin = req.headers.origin;
+//   if (allowedOrigins.includes(origin)) {
+//     res.header("Access-Control-Allow-Origin", origin);
+//   }
+//   res.header(
+//     "Access-Control-Allow-Headers",
+//     "Origin,X-Requested-With,Content-Type,Accept"
+//   );
+//   next();
+// });
+
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Content-Type");
   next();
 });
 
