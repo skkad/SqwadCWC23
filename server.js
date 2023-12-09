56abcb951e544da2e8572630b466c4e5e0e21fa0
skkad <skkad@gmail.com>
2023-12-09 17:22:18 +0530

    cors error fix

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ app.use(express.json());
 // });
 
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
   res.header("Access-Control-Allow-Headers", "Content-Type");
   next();
